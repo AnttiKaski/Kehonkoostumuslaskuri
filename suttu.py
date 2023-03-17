bb226c6e2f57e90be20966a8010503af5213c011
Mika Vainio <koti.mika.vainio@gmail.com>
2023-03-17 15:35:37 +0200

    Luotu lisää tiedostoesimerkkejä

--- suttu.py
+++ suttu.py
@@ -3,6 +3,7 @@
 import datetime  # Sisäänrakennettu kirjasto aikamääreille
 import json  # Sisäänrakennettu kirjasto JSON-objektien käsittelyä varten
 
+'''
 # Päiväyksen muodostaminen
 
 year = 2023
@@ -64,17 +65,27 @@ def timediff(t1, t2):
 
 kesto = timediff('10:00:00', '14:30:00')
 print(kesto)
+'''
+
+# Luodaan tyhjä lista pinon perustaksi
+jumppari_lista = []
 
 # Määritellään Python-sanakirja
 jumppari = {'nimi': 'Erkki', 'Pituus': 171, 'Paino': 75.5}
 jumppari2 = {'nimi': 'Essi', 'Pituus': 165, 'Paino': 61.2}
 
+# Lisätään jumpparit listaan
+jumppari_lista.append(jumppari)
+jumppari_lista.append(jumppari2)
+
+print(jumppari_lista)
+'''
 # Luodaan JSON-merkkijono (objekti)
 json_jumppari = json.dumps(jumppari)
 
 print(json_jumppari)
 
-''''
+
 # Luodaan tiedosto
 file_to_use = open('kuntoilijat.json', 'x')
 file_to_use.close()  # Suljetaan tiedosto operaation jälkeen
@@ -92,8 +103,17 @@ file_to_use = open('kuntoilijat.json', 'r')
 data = json.load(file_to_use)
 file_to_use.close()
 print(data)
-'''
+
 
 # Lisätään toinen JSON-objekti tiedoston loppuun
 with open('kuntoilijat.json', 'a') as file_to_use:
     json.dump(jumppari2, file_to_use)
+
+'''
+with open('kuntoilijat.json', 'w') as file_to_use:
+    json.dump(jumppari_lista, file_to_use, indent=4)
+
+with open('kuntoilijat.json', 'r') as file_to_use:
+    read_data = json.load(file_to_use)
+    last_data = read_data.pop()
+    print(last_data)
