fa22596355709b43ed855098401060dc87ffe634
Mika Vainio <koti.mika.vainio@gmail.com>
2023-03-17 13:59:27 +0200

    Koodia tallennuksiin

--- suttu.py
+++ suttu.py
@@ -1,7 +1,7 @@
 # Esimerkkejä päivämäärien, tiedostojen ja JSON-tietojen käytöstä
 
-import datetime # Sisäänrakennettu kirjasto aikamääreille
-import json # Sisäänrakennettu kirjasto JSON-objektien käsittelyä varten
+import datetime  # Sisäänrakennettu kirjasto aikamääreille
+import json  # Sisäänrakennettu kirjasto JSON-objektien käsittelyä varten
 
 # Päiväyksen muodostaminen
 
@@ -20,6 +20,8 @@ print(just_now)
 # Aika ero kahden päivämmäärän  välillä
 
 # Funktio. joka laskee päivämäärien eron päivinä
+
+
 def datediff(d1, d2):
     """Calculates the difference between two dates in days
 
@@ -32,13 +34,16 @@ def datediff(d1, d2):
     """
     d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
-    difference =  abs((d2 - d1).days)
+    difference = abs((d2 - d1).days)
     return difference
+
 
 ero = datediff('2023-03-17', '2023-01-20')
 print(ero)
 
 # Funktio, joka laskee kahden kellonajan välisen eron tunteina
+
+
 def timediff(t1, t2):
     """Calculates the difference between two time values
 
@@ -51,17 +56,44 @@ def timediff(t1, t2):
     """
     t1 = datetime.datetime.strptime(t1, "%H:%M:%S")
     t2 = datetime.datetime.strptime(t2, "%H:%M:%S")
-    seconds = abs((t2 - t1).seconds) # Vain sekunnit ja mikrosekunnit on tuettu timedeltassa
+    # Vain sekunnit ja mikrosekunnit on tuettu timedeltassa
+    seconds = abs((t2 - t1).seconds)
     hours = seconds / 3600
     return hours
+
 
 kesto = timediff('10:00:00', '14:30:00')
 print(kesto)
 
 # Määritellään Python-sanakirja
 jumppari = {'nimi': 'Erkki', 'Pituus': 171, 'Paino': 75.5}
+jumppari2 = {'nimi': 'Essi', 'Pituus': 165, 'Paino': 61.2}
 
 # Luodaan JSON-merkkijono (objekti)
 json_jumppari = json.dumps(jumppari)
 
 print(json_jumppari)
+
+''''
+# Luodaan tiedosto
+file_to_use = open('kuntoilijat.json', 'x')
+file_to_use.close()  # Suljetaan tiedosto operaation jälkeen
+
+
+# Kirjoitetaan tiedostoon JSON-objekti
+file_to_use = open('kuntoilijat.json', 'w')
+
+# Muutetaan sanakirja JSON-muotoon ja tallennetaan
+json.dump(jumppari, file_to_use)
+file_to_use.close() # Suljetaan tiedosto
+
+# Luetaan JSON-objekti tiedostosta
+file_to_use = open('kuntoilijat.json', 'r')
+data = json.load(file_to_use)
+file_to_use.close()
+print(data)
+'''
+
+# Lisätään toinen JSON-objekti tiedoston loppuun
+with open('kuntoilijat.json', 'a') as file_to_use:
+    json.dump(jumppari2, file_to_use)
