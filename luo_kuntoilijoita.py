fcfa84e912715981c6a83e44907910cd0ead0bda
Mika Vainio <koti.mika.vainio@gmail.com>
2023-03-21 13:53:23 +0200

    Konsolisovelluksen lopullinen koodi

--- luo_kuntoilijoita.py
+++ luo_kuntoilijoita.py
@@ -19,9 +19,10 @@ date_of_weighing = input('Punnituspäivä (vvvv-kk-pp): ')
 weight = questions.Question.ask_user_float(
     'Kuinka paljon painat (kg): ', True)[0]
 height = questions.Question.ask_user_float('Kuinka pitkä olet (cm): ', True)[0]
-age = questions.Question.ask_user_integer('Kuinka vanha olet', True)[0]
-gender = questions.Question.ask_user_integer(
-    'Sukupuoli 1 mies, 0 nainen: ', True)[0]
+age = questions.Question.ask_user_integer('Kuinka vanha olet: ', True)[0]
+allowed_genders = {'1': 1, '0': 0}
+gender = questions.Question.ask_user_dictionary(
+    'Sukupuoli 1 mies, 0 nainen: ', allowed_genders, True)[0]
 neck = questions.Question.ask_user_float(
     'Mikä on kaulanympäryksesi (cm): ', True)[0]
 waist = questions.Question.ask_user_float(
@@ -53,7 +54,13 @@ print(text_to_show)
 
 print('nimi', athlete.nimi, 'paino', athlete.paino)
 
-athlete_data = [] # Empty list for all athlete data
+# athlete_data = [] # Empty list for all athlete data
+
+# Read previous athlete_data from disk
+with open('athlete_data.json', 'r') as file:
+    athlete_data = json.load(file)
+    for item in athlete_data:
+        print('paino oli', item['paino'])
 
 # A dictionary for single weighing of an athlete
 athlete_data_row = {'nimi': athlete.nimi, 'pituus': athlete.pituus, 'paino': athlete.paino,
@@ -65,7 +72,8 @@ athlete_data.append(athlete_data_row)
 # SAVE DATA TO A FILE
 
 with open('athlete_data.json', 'w') as file:
-    json.dump(athlete_data, file)
+    json.dump(athlete_data, file, indent=4)
+
 
 # TODO: Read it from a JSON file
 
